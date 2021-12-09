bad16d287638babf58b6b45ae0b214ff1bcbd0c7
Volodymyr <83507366+volom@users.noreply.github.com>
2021-12-09 19:46:26 +0200

    Update win32com_METADATA.py

--- win32com_METADATA.py
+++ win32com_METADATA.py
@@ -7,10 +7,13 @@ current directory
 """
 import win32com.client as win32com
 import os
+import re
 
 list_files = []
 for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
     list_files += [os.path.join(dirpath, file) for file in filenames]
+
+list_files = [x for x in list_files if re.findall(r'\.(.*)', x)[0] in ['xlsx', 'xls', 'docx', 'doc', 'pptx', 'ppt']]
 
 for book in list_files:
 	try:
