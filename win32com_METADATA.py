cb245ad6f8c8c46b19e4aca41ffd1f8cb3a03e5f
Volodymyr <83507366+volom@users.noreply.github.com>
2021-12-09 13:36:53 +0200

    Update win32com_METADATA.py

--- win32com_METADATA.py
+++ win32com_METADATA.py
@@ -13,33 +13,31 @@ for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
     list_files += [os.path.join(dirpath, file) for file in filenames]
 
 for book in list_files:
-    try:
-        try:
-            excel = win32com.gencache.EnsureDispatch('Excel.Application')
-        	try:
-        	    wb = excel.Workbooks.Open(book, Local=True)
-        	except:
-        	    wb = excel.Workbooks.Open(book, Local=False)
-           	wb.RemovePersonalInformation = True
-           	wb.Close(SaveChanges=1)
-           	excel.Quit()
-        except:
-        	word = win32com.gencache.EnsureDispatch('Word.Application')
-        	wb = word.Documents.Open(book)
-        	wb.RemovePersonalInformation = True
-        	wb.Save()
-        	word.Quit()
-        finally:
-        	pp = win32com.gencache.EnsureDispatch('PowerPoint.Application')
-        	wb = pp.Presentations.Open(book, WithWindow=False)
-        	wb.RemovePersonalInformation = True
-        	wb.Save()
-        	wb.Close()
-    except:
-        pass
+	try:
+		try:
+			excel = win32com.gencache.EnsureDispatch('Excel.Application')
+			try:
+				wb = excel.Workbooks.Open(book, Local=True)
+			except:
+				wb = excel.Workbooks.Open(book, Local=False)
+			wb.RemovePersonalInformation = True
+			wb.Close(SaveChanges=1)
+			excel.Quit()
+		except:
+			word = win32com.gencache.EnsureDispatch('Word.Application')
+			wb = word.Documents.Open(book)
+			wb.RemovePersonalInformation = True
+			wb.Save()
+			word.Quit()
+		finally:
+			pp = win32com.gencache.EnsureDispatch('PowerPoint.Application')
+			wb = pp.Presentations.Open(book, WithWindow=False)
+			wb.RemovePersonalInformation = True
+			wb.Save()
+			wb.Close()
+	except:
+		pass
 
 print("Deleting metadata was done successfully!")
 while True:
     1 + 1
-    
-    
